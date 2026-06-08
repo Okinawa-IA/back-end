19d75446ed51c93743cb14869dfcee7c8e5d431a
Eduardo Leal Aruth <101681441+EduLeal@users.noreply.github.com>
2026-06-07 22:02:24 -0300

    feat: jogadas criticas

--- app/bot/agent.py
+++ app/bot/agent.py
@@ -4,6 +4,7 @@ from app.bot.heuristic import get_posicionamento_aleatorio, get_acao_aleatoria_t
 from app.bot.q_learning import escolher_acao_qlearning
 from app.bot.state_parser import get_professores_do_time
 from app.bot.q_learning import load_q_table, save_q_table
+from app.bot.state_parser import get_acoes_validas
 
 tabela_q = load_q_table()
 
@@ -25,13 +26,50 @@ def handle_setup_phase(payload: dict) -> dict:
     print(f"\n Jogando em: {posicao}")
     return posicao
 
+def buscar_jogada_critica(tabuleiro: list, nossos_professores: dict) -> dict:
+    #busca vitoria imediata ou bloqueio contra vitoria inimiga, caso nao achar retorna none
+    
+    jogada_defensiva = None
+
+    for prof_nome, pos_atual in nossos_professores.items():
+        acoes = get_acoes_validas(tabuleiro, pos_atual["row"], pos_atual["col"])
+        
+        for acao in acoes:
+            linha_destino = acao["move_to"]["row"]
+            col_destino = acao["move_to"]["col"]
+            linha_mentoria = acao["mentor_at"]["row"]
+            col_mentoria = acao["mentor_at"]["col"]
+
+            if tabuleiro[linha_destino][col_destino].get("level", 0) == 3: # ganhar
+                return {
+                    "professor": prof_nome,
+                    "move_to": acao["move_to"],
+                    "mentor_at": acao["mentor_at"]
+                }
+
+            
+            if tabuleiro[linha_mentoria][col_mentoria].get("level", 0) == 3: #bloquear vitoria do oponente
+                jogada_defensiva = {
+                    "professor": prof_nome,
+                    "move_to": acao["move_to"],
+                    "mentor_at": acao["mentor_at"]
+                }
+
+    return jogada_defensiva
+
 def handle_turn_phase(payload: dict) -> dict:
     tabuleiro = payload.get("board", [])
     nossos_professores = get_professores_do_time(payload)
     
     if not nossos_professores:
         return get_acao_aleatoria_turno(payload) 
-        
+    
+    jogada_critica = buscar_jogada_critica(tabuleiro, nossos_professores)
+    if jogada_critica:
+        print(f"\nJogada critica encontrada: {json.dumps(jogada_critica)}")
+        return jogada_critica
+
+    #nao achou jogada critica - vai pro qlearning
     prof_escolhido_nome = random.choice(list(nossos_professores.keys()))
     pos_prof = nossos_professores[prof_escolhido_nome]
 
